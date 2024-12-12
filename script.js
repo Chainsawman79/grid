6df724eae37b27eafbc31cac48a5938f17944f0e
Charchit73 <148005866+Chainsawman79@users.noreply.github.com>
2024-12-12 15:42:10 +0530

    JavaScript Practice

---
+++ script.js
@@ -0,0 +1,123 @@
+/*document.write("<h1>This is My first javaScript Page</h1>");
+function sayHello(){alert("Hello World")}
+var pi=3.14;
+var person="are you GOD";
+var ans='Yes I am!';
+document.write(pi+"<br>");
+document.write(person+"<br>");
+document.write(ans+"<br>");*/
+// function myFunction()
+// {
+//     var carname="Welcome";
+//     document.getElementById("demo").innerHTML=carname;
+// }
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+//  num=false;
+// console.log(num);
+// console.log(typeof(num));
+//  num="Sanjeev Soni";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+// var person={firstName:"John", lastName:"Doe,id=5566",
+// getinfo:function()
+// {
+//     return this.firstName+" "+this.lastName;
+// }
+// };
+// document.getElementById("demo").innerHTML=person.getinfo();
+//  var person={
+//     name:'Charchit',
+//     age:20,
+//     iSMarried:false,
+//     address:{
+//         street:'Gwalior',
+//         flatNo:201
+//     }
+// };
+// console.log(person);
+// var preson2=new Object();
+// preson2.name='Cahrchit';
+// preson2.age=20;
+// preson2.isMarried=true;
+// preson2.address={};
+// preson2.address.street='Cahrchit';
+// preson2.address['flatNo']=33;
+// console.log(preson2);
+// function myFunction()
+// {
+//     var carname="Volvo";
+//     document.getElementById("demo1").innerHTML=typeof carname+" "+carname;
+// }
+
+// myFunction();
+// document.getElementById("demo2").innerHTML=typeof carname;
+// 
+// var locale={
+//     europe:function(){
+//         var myFriend='Monique';
+//         var france=function(){
+//         var paris=function(){
+//         console.log(myFriend);
+//     };
+//     paris();
+// };
+// france();
+//     }
+// };
+// locale.europe();
+// function show(){
+//     var a="a is a local outer variable";
+//     document.write(a+"<br>");
+//     function disk(){
+//         var b="b is a local inner variable";
+//         document.write(b+"<br>");
+//     }
+//     document.write("h"+"<br>");
+//     disk();
+// }
+// show();
+// 
+// var myVar="global";
+// function check(){
+//     var myVar="local";
+//     document.write(myVar);
+// }
+// var name='John';
+// function first()
+// {
+//     var a='Hello';
+//     second();
+//     var x=a+ name; 
+//     console.log(x);
+// }
+// function second()
+// {
+//     var b='Hi';
+//     third();
+//     var y=b+ name;
+//     console.log(y); 
+// }
+// function third()
+// {
+//     var c='Hey!';
+//     var z=c+ name; 
+//     console.log(z);
+// }
+// first();
+var age=20;
+if(age>18)
+{
+    document.write("<b>Eligible</b>");
+}
+
+
+
+
